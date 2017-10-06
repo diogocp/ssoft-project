93ac1bd365999591678a9b185c11945a7b683141
Diogo Pereira <diogo.pereira@tecnico.ulisboa.pt>
2017-10-06 09:52:38 +0100

    Listener example

--- analyzer.py
+++ analyzer.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 
 import sys
-from antlr4 import *
+import antlr4
 from parser.PHPLexer import PHPLexer
 from parser.PHPParser import PHPParser
+from parser.PHPParserListener import PHPParserListener
+
+class PhpPrinter(PHPParserListener):
+    def enterHtmlElement(self, ctx):
+        print("Entering HTML element", ctx)
+    def enterPhpBlock(self, ctx):
+        print("Entering PHP block", ctx)
+    def enterExpressionStatement(self, ctx):
+        print("Entering expression", ctx)
+    def enterEchoStatement(self, ctx):
+        print("Entering echo", ctx)
+    def enterFunctionCall(self, ctx):
+        print("Entering function call", ctx)
 
 def main(argv):
-    input = FileStream(argv[1])
-    lexer = PHPLexer(input)
-    stream = CommonTokenStream(lexer)
+    lexer = PHPLexer(antlr4.FileStream(argv[1]))
+    stream = antlr4.CommonTokenStream(lexer)
     parser = PHPParser(stream)
-    tree = parser.phpBlock()
+
+    tree = parser.htmlElementOrPhpBlock()
+
+    printer = PhpPrinter()
+    walker = antlr4.ParseTreeWalker()
+    walker.walk(printer, tree)
 
 if __name__ == '__main__':
     main(sys.argv)
